97bc04f260a7d9f895b2920063ad5995b80592ae
hungqtr <22130094@st.hcmuaf.edu.vn>
2024-11-04 00:00:58 +0700

    update user profile

--- frontEnd/src/styles/style-page/home.js
+++ frontEnd/src/styles/style-page/home.js
@@ -35,11 +35,11 @@ menuItems.forEach(item => {
 $(document).ready(() => {
     const nav_items= $('.nav_item');
     const content = $('#body iframe')
-    console.log(content)
-    console.log($(nav_items[0]).attr('data-src'));
+    // console.log(content)
+    // console.log($(nav_items[0]).attr('data-src'));
 
     Array.from(nav_items).forEach(nav_item => {
-        console.log($(nav_item).attr('data-src'));
+        // console.log($(nav_item).attr('data-src'));
         nav_item.addEventListener('click', () => {
            content.attr('src', $(nav_item).attr('data-src'));
         })
