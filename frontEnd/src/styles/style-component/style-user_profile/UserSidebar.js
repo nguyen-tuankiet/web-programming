d37028d52d36d25dd5b0fc2c9381c145dd5ff55a
hungqtr <22130094@st.hcmuaf.edu.vn>
2024-11-10 01:55:46 +0700

    update user profile

--- frontEnd/src/styles/style-component/style-user_profile/UserSidebar.js
+++ frontEnd/src/styles/style-component/style-user_profile/UserSidebar.js
@@ -1,49 +1,62 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
-    const myAccount = document.querySelector(".edit_profile");
+    const items = Array.from(document.querySelectorAll('.item'));
+
+    const myAccount = items[0];
+    console.log(myAccount);
     const subMenu = document.querySelector(".menu_sub_item");
     const subItem = document.querySelectorAll(".sub_items");
-    const mainContent = document.getElementById('main_content');
 
 
-    console.log(subItem);
-    myAccount.addEventListener("click", () => {
+    items.forEach(item => {
+        item.addEventListener('click', (e) => {
 
-        if (subMenu.classList.contains('open')) {
-            subMenu.classList.remove('open');
 
-        } else {
-            Array.from(subItem)[0].classList.add('selected');
-            subMenu.classList.add('open');
-            console.log(Array.from(subItem)[0])
-
-        }
-    })
-
-    subItem.forEach((item) => {
-
-        item.addEventListener("click", (event) => {
-            event.stopPropagation();
-            subItem.forEach((item) => {
-                // item.classList.remove('selected');
+            items.forEach((i) => {
+                i.classList.remove('selected');
             })
-            item.classList.add('selected');
 
-            console.log(item);
 
-            const link = item.querySelector('.item_link[data-src]');
-            if (link) {
-                const dataSrc = link.getAttribute('data-src'); //    Lấy giá trị tu data-src
-                event.preventDefault();
-                window.parent.postMessage({type: 'openUserProfile', src: dataSrc}, '*');
+            if (item === myAccount && !subMenu.classList.contains('open')) {
+                console.log(item);
+                subMenu.classList.add('open');
             }
-
+            else {
+                subMenu.classList.remove('open');
+            }
+            item.classList.add('selected');
+        }
+        )
 
         })
 
+
+
+        subItem.forEach((item) => {
+            Array.from(subItem)[0].classList.add('selected');
+
+            item.addEventListener("click", (event) => {
+                event.stopPropagation();
+                subItem.forEach((item) => {
+                    item.classList.remove('selected');
+                })
+                item.classList.add('selected');
+
+                console.log(item);
+
+                const link = item.querySelector('.item_link[data-src]');
+                if (link) {
+                    const dataSrc = link.getAttribute('data-src'); //    Lấy giá trị tu data-src
+                    event.preventDefault();
+                    window.parent.postMessage({type: 'openUserProfile', src: dataSrc}, '*');
+                }
+
+
+            })
+
+        })
+
+
     })
-
-
-})
 
