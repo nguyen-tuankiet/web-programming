9ebac97f07cbbb7a6235a8c4eae32d4343c3b395
hungqtr <22130094@st.hcmuaf.edu.vn>
2024-11-10 14:04:39 +0700

    update user profile - navigation

--- frontEnd/src/styles/style-component/style-user_profile/UserSidebar.js
+++ frontEnd/src/styles/style-component/style-user_profile/UserSidebar.js
@@ -4,59 +4,53 @@ document.addEventListener('DOMContentLoaded', () => {
     const items = Array.from(document.querySelectorAll('.item'));
 
     const myAccount = items[0];
-    console.log(myAccount);
+
     const subMenu = document.querySelector(".menu_sub_item");
-    const subItem = document.querySelectorAll(".sub_items");
+    const nav = document.querySelectorAll(".nav");
 
 
     items.forEach(item => {
         item.addEventListener('click', (e) => {
-
-
-            items.forEach((i) => {
-                i.classList.remove('selected');
-            })
-
-
-            if (item === myAccount && !subMenu.classList.contains('open')) {
-                console.log(item);
-                subMenu.classList.add('open');
+                items.forEach((i) => {
+                    i.classList.remove('selected');
+                })
+                if (item === myAccount && !subMenu.classList.contains('open')) {
+                    subMenu.classList.add('open');
+                } else {
+                    subMenu.classList.remove('open');
+                }
+                item.classList.add('selected');
             }
-            else {
-                subMenu.classList.remove('open');
-            }
-            item.classList.add('selected');
-        }
         )
 
-        })
+    })
 
 
+    nav.forEach((item) => {
+        Array.from(nav)[0].classList.add('selected');
 
-        subItem.forEach((item) => {
-            Array.from(subItem)[0].classList.add('selected');
-
-            item.addEventListener("click", (event) => {
-                event.stopPropagation();
-                subItem.forEach((item) => {
-                    item.classList.remove('selected');
-                })
-                item.classList.add('selected');
-
-                console.log(item);
-
-                const link = item.querySelector('.item_link[data-src]');
-                if (link) {
-                    const dataSrc = link.getAttribute('data-src'); //    Lấy giá trị tu data-src
-                    event.preventDefault();
-                    window.parent.postMessage({type: 'openUserProfile', src: dataSrc}, '*');
-                }
-
-
+        item.addEventListener("click", (event) => {
+            event.stopPropagation();
+            nav.forEach((item) => {
+                item.classList.remove('selected');
             })
+            item.classList.add('selected');
+
+            console.log(item);
+
+            const link = item.querySelector('.item_link[data-src]');
+            console.log(link)
+            if (link) {
+                const dataSrc = link.getAttribute('data-src'); //    Lấy giá trị tu data-src
+                event.preventDefault();
+                window.parent.postMessage({type: 'openUserProfile', src: dataSrc}, '*');
+            }
+
 
         })
-
 
     })
+
+
+})
 
