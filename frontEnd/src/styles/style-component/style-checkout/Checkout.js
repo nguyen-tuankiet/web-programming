1967dd3fe40cc4c9a0582473cea9793a09bf234e
hungqtr <22130094@st.hcmuaf.edu.vn>
2024-11-14 23:34:59 +0700

    update navigate

---
+++ frontEnd/src/styles/style-component/style-checkout/Checkout.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    const back = $('#back')
+    back.on('click', function () {
+        const message = {
+            type: 'navigate',
+            data: back.attr('data-src'),
+        }
+        window.parent.postMessage(message, '*');
+    })
+
+
+    const pay = $('#pay')
+    pay.on('click', function () {
+        const message = {
+            type: 'navigate',
+            data: pay.attr('data-src'),
+        }
+        console.log(message);
+        window.parent.postMessage(message, '*');
+    })
+})
