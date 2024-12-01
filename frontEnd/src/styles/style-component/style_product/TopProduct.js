adafc20e5580772d0fe4c03fba3a83ab0a147c4a
Category <tuankietcoder2211@gmail.com>
2024-12-01 13:22:27 +0700

    fix

--- frontEnd/src/styles/style-component/style_product/TopProduct.js
+++ frontEnd/src/styles/style-component/style_product/TopProduct.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const addToCartButtons = document.querySelectorAll(".btn.add");
+    const notification = document.getElementById("cart-notification");
 
+    addToCartButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            notification.classList.remove("hidden");
+            notification.classList.add("show");
+
+            setTimeout(() => {
+                notification.classList.remove("show");
+                notification.classList.add("hidden");
+            }, 3000);
+        });
+    });
+});
 
 
