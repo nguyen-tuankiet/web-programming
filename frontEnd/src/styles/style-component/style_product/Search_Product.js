31d64b37be404f18f63e8734a25042f6229a17c9
Category <tuankietcoder2211@gmail.com>
2024-12-03 00:29:58 +0700

    đủ để báo cáo rồi

---
+++ frontEnd/src/styles/style-component/style_product/Search_Product.js
@@ -0,0 +1,7 @@
+document.getElementById('buy_now').addEventListener('click', function() {
+    window.location.href = '../../../component/Checkout/Checkout.html';
+});
+
+document.getElementById('name').addEventListener('click', function() {
+    window.location.href = '../../../component/product_detail/Product-detail.html';
+});
