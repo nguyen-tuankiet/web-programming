38b4dc5f7164b2863fa05452461de69bcf35c98e
VSiJs <22130234@st.hcmuaf.edu.vn>
2024-12-28 23:59:53 +0700

    UP jsp Product

---
+++ backend/src/main/java/com/example/backend/controller/ProductDetail/ProductController.java
@@ -0,0 +1,19 @@
+package com.example.backend.controller.ProductDetail;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "ProductController", value = "/ProductController")
+public class ProductController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
--- backend/src/main/webapp/Product-detail/footer.jsp
+++ backend/src/main/webapp/Product-detail/footer.jsp
@@ -12,7 +12,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Footer</title>
-    <link rel="stylesheet" href="<%= request.getContextPath() %>/styles/style-component/style-home/footer.css">
+    <link rel="stylesheet" href="<%= request.getContextPath() %>/static/style-component/style-home/footer.css">
 </head>
 <body>
 
