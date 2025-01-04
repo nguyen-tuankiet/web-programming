35700b6e50edcfd70400f12fe624fc7599b08cfb
hungqtr <22130094@st.hcmuaf.edu.vn>
2025-01-04 18:36:03 +0700

    update

--- backend/src/main/webapp/static/style-component/sytle-cart/Cart.js
+++ backend/src/main/webapp/static/style-component/sytle-cart/Cart.js
@@ -10,4 +10,108 @@ $(document).ready(function () {
         console.log(message);
         window.parent.postMessage(message, '*');
     })
+
+
+
+
+
+
+//     Increase quantity
+
+    const product= $('.product-item')
+
+    window.onload = function () {
+        product.each(function () {
+
+        let price = $(this).find('.price');
+        let quantity = ($(this).find('.num'));
+        let increment = $(this).find('#increment');
+        let decrement = $(this).find('#decrement');
+
+
+        updatePrice(price, quantity);
+
+
+
+        increment.on('click', function () {
+            increaseQuantity( $(this), quantity, price);
+        })
+
+
+
+
+
+
+
+
+
+       })
+    }
+
+
+    function updatePrice(price, quantity) {
+
+
+        let price_value = parseInt(price.attr('data-price'));
+        let quantity_value = parseInt(quantity.attr('data-quantity'));
+        let total = price_value * quantity;
+
+        let formatted = new Intl.NumberFormat('vi-VN').format(total);
+        price.text(formatted + 'VND');
+
+
+
+
+
+    }
+
+
+    function increaseQuantity( product, quantity , price ) {
+
+
+        let newQuantity =quantity +1;
+
+        updatePrice(price, newQuantity);
+        quantity.text(newQuantity);
+
+    }
+
+
+
+    function updateQuantity( product, quantity ) {
+        $.ajax({
+            url: '/backend_war_exploded/update-quantity' ,
+            method: 'POST',
+            data: {
+                product: product,
+                quantity: quantity
+            },
+            success: function (result) {
+                console.log(result);
+            },
+            error: function (xhr, status, error) {
+                console.log(xhr.responseText);
+            }
+
+
+        })
+    }
+
+
+
+
+
+
+
+
+    // decrement.on('click', function () {
+    //     if (parseInt(quantity.text()) > 1) {
+    //         let newQuantity = parseInt($('#quantity').text()) -1;
+    //         quantity.text(newQuantity);
+    //     }
+    //
+    // })
+
 })
+
+
