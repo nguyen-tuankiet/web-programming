99fc14e14a8212df330e08ef5fd3fec4b12c051a
hungqtr <22130094@st.hcmuaf.edu.vn>
2025-01-06 00:20:19 +0700

    update

--- backend/src/main/java/com/example/backend/model/DAO/CardDAO.java
+++ backend/src/main/java/com/example/backend/model/DAO/CardDAO.java
@@ -5,12 +5,14 @@ import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 
+import java.util.List;
+
 @RegisterConstructorMapper(Card.class)
 public interface CardDAO {
 
     @SqlQuery(value = "SELECT *\n" +
             "FROM card\n" +
-            "WHERE userId = :id;")
-    Card getCardById(@Bind("id") Integer id);
+            "WHERE userId = :userId;")
+    List<Card> getCardByUserId(@Bind("userId") Integer userId);
 
 }
