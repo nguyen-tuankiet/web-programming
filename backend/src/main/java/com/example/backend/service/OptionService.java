6c16407d09b6eebf4c8f008214518ddf8989f30d
hungqtr <22130094@st.hcmuaf.edu.vn>
2025-01-13 16:04:40 +0700

    increase stock

--- backend/src/main/java/com/example/backend/service/OptionService.java
+++ backend/src/main/java/com/example/backend/service/OptionService.java
@@ -1,6 +1,8 @@
 package com.example.backend.service;
 
+import com.example.backend.Connection.DBConnection;
 import com.example.backend.model.DAO.OptionDAO;
+import com.example.backend.model.Options;
 import org.jdbi.v3.core.Jdbi;
 
 public class OptionService {
@@ -13,5 +15,25 @@ public class OptionService {
     public int createOptions(Integer productId, Integer price, Integer stock) {
         return optionDao.createOption(productId, price, stock);
     }
+
+
+    public Options getOptionById(Integer id) {
+        return optionDao.getOptionById(id);
+    }
+
+
+    public Boolean updateStock(Integer id, Integer stock) {
+        return optionDao.updateStock(id, stock);
+    }
+
+
+    public static void main(String[] args) {
+        OptionService  optionService = new OptionService(DBConnection.getJdbi());
+        System.out.println(optionService.getOptionById(1));
+        System.out.println(optionService.updateStock(1, 200));
+    }
+
+
+
 
 }
