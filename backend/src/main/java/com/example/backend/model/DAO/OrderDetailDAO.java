52d06a8ce7f9b23f2f88fc4bcfc73cd6b1d1f378
VSiJs <22130234@st.hcmuaf.edu.vn>
2025-01-13 14:28:22 +0700

    add order

--- backend/src/main/java/com/example/backend/model/DAO/OrderDetailDAO.java
+++ backend/src/main/java/com/example/backend/model/DAO/OrderDetailDAO.java
@@ -3,13 +3,15 @@ package com.example.backend.model.DAO;
 import com.example.backend.model.OrderDetail;
 import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
+import java.util.List;
+@RegisterConstructorMapper(OrderDetail.class)
 public interface OrderDetailDAO {
 
     @SqlUpdate(value ="INSERT  INTO order_detail ( orderId, productId, quantity, total, optionId)\n" +
             " VALUE (:orderId, :productId, :quantity, :total, :optionId)")
-    @RegisterConstructorMapper(OrderDetail.class)
     Boolean addOrderDetail(
             @Bind("orderId") Integer orderId,
             @Bind("productId") Integer productId,
@@ -18,5 +20,31 @@ public interface OrderDetailDAO {
             @Bind("optionId") Integer optionId
 
     );
+
+
+
+    @SqlQuery("SELECT name FROM product WHERE id = :productId")
+    String getProductNameById(@Bind("productId") Integer productId);
+
+
+    @SqlQuery("SELECT quantity FROM order_detail WHERE id = :orderDetailId")
+    Integer getQuantityByOrderDetailId(@Bind("orderDetailId") Integer orderDetailId);
+
+    @SqlQuery("SELECT orderStatus FROM orders WHERE id = :orderId")
+    String getOrderStatusByOrderId(@Bind("orderId") Integer orderId);
+
+    @SqlQuery(value = " SELECT\n" +
+            "        od.id AS id,\n" +
+            "        od.orderId AS orderId,\n" +
+            "        od.productId AS productId,\n" +
+            "        od.quantity AS quantity,\n" +
+            "        od.total AS total,\n" +
+            "        p.name AS productName\n" +
+            "    FROM order_detail od\n" +
+            "    JOIN products p ON od.productId = p.id\n" +
+            "    WHERE od.orderId = :orderId")
+    @RegisterConstructorMapper(OrderDetail.class)
+    List<OrderDetail> getOrderDetailsByOrderId(@Bind("orderId") Integer orderId);
+
 
 }
