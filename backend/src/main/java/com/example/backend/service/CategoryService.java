4073e029a5904f950fd579cea33ffae5585bcb55
hungqtr <22130094@st.hcmuaf.edu.vn>
2025-01-14 20:45:47 +0700

    Update CategoryService.java

--- backend/src/main/java/com/example/backend/service/CategoryService.java
+++ backend/src/main/java/com/example/backend/service/CategoryService.java
@@ -4,6 +4,8 @@ import com.example.backend.model.Category;
 import com.example.backend.model.DAO.CategoryDAO;
 import org.jdbi.v3.core.Jdbi;
 
+import java.util.List;
+
 public class CategoryService {
     private final CategoryDAO categoryDAO;
 
@@ -11,7 +13,32 @@ public class CategoryService {
         this.categoryDAO = jdbi.onDemand(CategoryDAO.class);
     }
 
-    public void addCategory(Category category) {
-        categoryDAO.addCategory(category.getName());
+    public List<Category> getAllCategories() {
+        return categoryDAO.getAllCategories();
+    }
+
+//    public Category getCategoryById(Integer id) {
+//        return categoryDAO.getCategoryById(id);
+//    }
+
+    public Category getCategoryById(Integer id) {
+        Category category = categoryDAO.getCategoryById(id);
+        if (category == null) {
+            throw new IllegalArgumentException("Category not found");
+        }
+        return category;
+    }
+
+    public Category createCategory(String name) {
+        int id = categoryDAO.createCategory(name);
+        return categoryDAO.getCategoryById(id);
+    }
+
+    public void updateCategory(Integer id, String name) {
+        categoryDAO.updateCategory(id, name);
+    }
+
+    public void deleteCategory(Integer id) {
+        categoryDAO.deleteCategory(id);
     }
 }
