bf032774dfcb076ae2e72b2b7aa92bb96a8f6bfd
hungqtr <22130094@st.hcmuaf.edu.vn>
2025-01-14 20:30:18 +0700

    Update CategoryDAO.java

--- backend/src/main/java/com/example/backend/model/DAO/CategoryDAO.java
+++ backend/src/main/java/com/example/backend/model/DAO/CategoryDAO.java
@@ -3,11 +3,29 @@ package com.example.backend.model.DAO;
 import com.example.backend.model.Category;
 import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
-@RegisterConstructorMapper(Category.class)
+import java.util.List;
 
+@RegisterConstructorMapper(Category.class)
 public interface CategoryDAO {
+
+    @SqlQuery("SELECT * FROM categories")
+    List<Category> getAllCategories();
+
+    @SqlQuery("SELECT * FROM categories WHERE id = :id")
+    Category getCategoryById(@Bind("id") Integer id);
+
     @SqlUpdate("INSERT INTO categories (name) VALUES (:name)")
-    void addCategory(@Bind("name") String name);
+    @GetGeneratedKeys("id")
+    int createCategory(@Bind("name") String name);
+
+
+    @SqlUpdate("UPDATE categories SET name = :name WHERE id = :id")
+    void updateCategory(@Bind("id") Integer id, @Bind("name") String name);
+
+    @SqlUpdate("DELETE FROM categories WHERE id = :id")
+    void deleteCategory(@Bind("id") Integer id);
 }
