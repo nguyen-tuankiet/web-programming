f5fa9a7c7e3500bb232f24fc1eb13b8de1f9245a
hungqtr <22130094@st.hcmuaf.edu.vn>
2025-01-15 11:26:17 +0700

    show card

--- backend/src/main/java/com/example/backend/controller/user/card/UserCardController.java
+++ backend/src/main/java/com/example/backend/controller/user/card/UserCardController.java
@@ -1,17 +1,30 @@
 package com.example.backend.controller.user.card;
+import com.example.backend.Connection.DBConnection;
+import com.example.backend.model.Card;
+import com.example.backend.model.User;
+import com.example.backend.service.CardService;
+import com.example.backend.service.UserService;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "UserCardController", value = "/user-card")
 public class UserCardController extends HttpServlet {
-    
+
+    CardService cardService = new CardService(DBConnection.getJdbi());
+    UserService userService = new UserService(DBConnection.getJdbi());
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
 
-        //TODO: Get cards by userID
+        HttpSession session = request.getSession();
+        Integer userId = (Integer) session.getAttribute("userId");
+        User user = userService.getUserById(userId);
+        List<Card> cards= cardService.getCartByUserId(userId);
+        request.setAttribute("cards", cards);
+        request.setAttribute("user", user);
 
         request.getRequestDispatcher("user/user-card.jsp").forward(request, response);
 
