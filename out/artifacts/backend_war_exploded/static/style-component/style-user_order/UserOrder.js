0c3849252418411b4bf26fcc632c5ea5984b4b3a
qhung.edu <2213004@st.hcmuaf.edu.vn>
2025-03-03 22:58:56 +0700

    Ignore out/ directory

--- out/artifacts/backend_war_exploded/static/style-component/style-user_order/UserOrder.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    window.addEventListener('message', function (event) {
-        if (event.data.type === 'openOrderHistory') {
-            window.parent.postMessage(event.data, '*')
-        }
-    })
-})
