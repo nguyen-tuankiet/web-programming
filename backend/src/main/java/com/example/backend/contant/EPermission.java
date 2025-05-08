85ac7ba5cc24d7c909ff18a1bed3255956e5dcdf
hungqtr <22130094@st.hcmuaf.edu.vn>
2025-05-08 12:58:04 +0700

    member

---
+++ backend/src/main/java/com/example/backend/contant/EPermission.java
@@ -0,0 +1,6 @@
+package com.example.backend.contant;
+
+public enum EPermission {
+    OVERVIEW,
+
+}
---
+++ backend/src/main/java/com/example/backend/model/Role.java
@@ -0,0 +1,11 @@
+package com.example.backend.model;
+
+import com.example.backend.contant.ERole;
+
+public class Role {
+    Long id;
+    ERole roleType;
+    String name;
+    String description;
+
+}
